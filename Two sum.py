2d22e505318df35f09619286d7398a6325249aea
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2022-11-17 15:16:22 -0500

    Create Two sum.py

---
+++ Two sum.py
@@ -0,0 +1,12 @@
+#takes a set of numbers and a target number. Returns the indices of two numbers from the set that add up to the target number
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+print(Solution().twoSum([2, 7, 11, 15], 9)) #
+
+
+
