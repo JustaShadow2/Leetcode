8f61a884caa24e3e7ed9bc6f6efc042fb6f006e4
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2022-11-17 20:40:25 -0500

    Update Easy Questions.py

--- Easy Questions.py
+++ Easy Questions.py
@@ -4,14 +4,28 @@ class Solution(object):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
-
+                    return [i, j]               
 print(Solution().twoSum([2, 7, 11, 15], 9)) 
+
+
 
 class Solution(object):
     def isPalindrome(self, x):
         x = str(x)
         if x == x[::-1]:
-            return True
-        
+            return True        
 print(Solution().isPalindrome(121))
+
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+print(Solution().removeDuplicates([1, 1, 2]))
