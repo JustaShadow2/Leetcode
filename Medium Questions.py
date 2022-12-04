2ead2ffd23d4a7009947e873276f7ffd023a3739
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2022-12-03 21:22:00 -0500

    Update Medium Questions.py

--- Medium Questions.py
+++ Medium Questions.py
@@ -32,3 +32,16 @@ class Solution(object): # Given the coordinates of two rectilinear rectangles in
         overlap = max(min(ax2, bx2) - max(ax1, bx1), 0) * max(min(ay2, by2) - max(ay1, by1), 0)
         # return the sum of the areas minus the overlap
         return area1 + area2 - overlap
+    
+    
+#integer to roman numeral (i cant believe that a half hardcoded thing worked)
+class Solution(object):
+    def intToRoman(self, num):
+        roman = {1:'I', 4:'IV', 5:'V', 9:'IX', 10:'X', 40:'XL', 50:'L', 90:'XC', 100:'C', 400:'CD', 500:'D', 900:'CM', 1000:'M'}
+        result = ''
+        for i in sorted(roman.keys(), reverse=True):
+            while num >= i:
+                result += roman[i]
+                num -= i
+        return result
+print(Solution().intToRoman(1994))
