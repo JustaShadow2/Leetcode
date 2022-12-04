c3a37dc72b9f8b9beccbc26c41bb694c1bdeab0f
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2022-12-03 21:19:07 -0500

    Update Easy Questions.py

--- Easy Questions.py
+++ Easy Questions.py
@@ -29,3 +29,55 @@ class Solution(object):
                 nums[i] = nums[j]
         return i + 1
 print(Solution().removeDuplicates([1, 1, 2]))
+
+
+
+#hardcoded version, but much easier to understand
+'''class Solution(object):
+    def romanToInt(self, s):
+        total = 0
+        for i in s:
+            if i == 'I':
+                total += 1
+            elif i == 'V':
+                total += 5
+            elif i == 'X':
+                total += 10
+            elif i == 'L':
+                total += 50
+            elif i == 'C':
+                total += 100
+            elif i == 'D':
+                total += 500
+            elif i == 'M':
+                total += 1000
+
+        if 'IV' in s:
+            total -= 2
+        if 'IX' in s:
+            total -= 2
+        if 'XL' in s:
+            total -= 20
+        if 'XC' in s:
+            total -= 20
+        if 'CD' in s:
+            total -= 200
+        if 'CM' in s:
+            total -= 200
+        return total'''
+
+
+
+# imagine not hardcoding it, but using a dictionary instead
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        total = 0
+        for i in range(len(s)):
+            if i > 0 and roman[s[i]] > roman[s[i-1]]:
+                total += roman[s[i]] - 2 * roman[s[i-1]]
+            else:
+                total += roman[s[i]]
+        return total
+
+print(Solution().romanToInt('MCMXCIV'))
